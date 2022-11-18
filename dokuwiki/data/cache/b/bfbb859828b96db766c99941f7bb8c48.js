c6d2f44f8c4d7c80f782e5d9069e9249a537e567
Alex <alexander.zieschang@gmail.com>
2022-11-18 16:28:53 +0100

    first upload

---
+++ dokuwiki/data/cache/b/bfbb859828b96db766c99941f7bb8c48.js
@@ -0,0 +1,1 @@
+SimpleSVG._loaderCallback({"prefix":"mdi","icons":{"home":{"body":"<path d=\"M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5z\" fill=\"currentColor\"\/>"}},"aliases":[],"width":24,"height":24});
